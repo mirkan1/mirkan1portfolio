c6c9d6881ff74bbf3aa16bef548bc1b62edbdc3c
mirkan1 <mirkanbaba1@gmail.com>
2020-04-09 04:09:37 -0500

    Asda

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/index.css';
-import App from './App';
+// import './css/index.css';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h1>Hello World!</h1>
+    {/* <App /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
