b47b4ed84601a49a9d347074caaaa0d125584d77
mirkan1 <mirkanbaba1@gmail.com>
2020-04-09 04:13:51 -0500

    dep

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Hello World!</h1>
+    <h1>Hello, World</h1>
     {/* <App /> */}
   </React.StrictMode>,
   document.getElementById('root')
