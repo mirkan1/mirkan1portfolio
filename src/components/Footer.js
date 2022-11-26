0198a6a7a91e7176db7647dacb49152aeba2e6c0
mirkan1 <mirkanbaba1@gmail.com>
2022-11-26 00:36:42 -0500

    a tag fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -108,15 +108,10 @@ export default function Footer() {
         <Container className="footer">
           <div className="row1">
             <h4>About Me</h4>
-            <p>I'm Renas Kilic, 
-              Freelancer from New Orleans. 
-              Developing and maintaining websites for many years. 
-              Specialised in secure transcations including web3 transations</p>
+            <p>I'm Developing, maintaining, and updating software applications.</p>
           </div>
           <div className="row2">
-            <h4 hidden>Contact Me</h4>
-            <p hidden>Mobile: NOPE</p>
-            <p>Email:<a href="mailto:mirkanbaba1@gmail.com?subject=Hello Mirkan, I clicked the button from your website;">&nbsp;mirkan</a></p>
+            <p><a href="mailto:mirkanbaba1@gmail.com">Mail me about anything</a></p>
             <a className="hvr-underline-from-left" href="/privacy" hidden>Privacy Policy</a>
           </div>
         </Container>
@@ -124,11 +119,10 @@ export default function Footer() {
           <hr/>
           <div className="footer-b">
             <a href="https://github.com/mirkan1" target="_blank" rel="noopener noreferrer"><img src={githubLogo} height="32" width="32" alt="media-link"></img>&nbsp;&nbsp;github</a>
-            <a href="https://www.facebook.com/R.mirkan" target="_blank" rel="noopener noreferrer"><img src={facebookLogo} height="32" width="32" alt="media-link" hidden></img>&nbsp;&nbsp;facebook</a>
             <a href="https://twitter.com/mirkanOFmirkan" target="_blank" rel="noopener noreferrer"><img src={twitterLogo} height="32" width="32" alt="media-link"></img>&nbsp;&nbsp;twitter</a>
             <a href="https://linkedin.com/in/raqux/" target="_blank" rel="noopener noreferrer"><img src={linkedinLogo} height="32" width="32" alt="media-link"></img>&nbsp;&nbsp;linkedin</a>
           </div>
-            <p>R. M. Kilic © 2022/04/23</p>
+            <p>Renas Mirkan Kilic © 2023</p>
         </div>
       </div>
     </Styles>
