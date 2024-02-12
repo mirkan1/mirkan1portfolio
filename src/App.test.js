e0edb2d349eaf34f0f7d33b964af1c8ee966964e
mirkan-samsung-wsl <mirkanbaba1@gmail.com>
2024-02-12 13:16:01 +0300

    Refactor App.test.js to test Work component for current year

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+//import App from './App';
+import Work from './pages/Work';
 
-test('if the copyright is for latest year', () => {
+test('if I have at least one project for this  year', () => {
   const currentYear = new Date().getFullYear().toString()
-  const { getByText } = render(<App />);
-  const linkElement = getByText(currentYear, { exact: false }); 
-  expect(linkElement).toBeInTheDocument();
+  // const getByTextApp = render(<App />).getByText;
+  const getByTextWork = render(<Work />).getByText;
+  // const linkElementApp = getByTextApp(currentYear, { exact: false });
+  const linkElementWork = getByTextWork(currentYear, { exact: false });
+  expect(linkElementWork).toBeInTheDocument();
 });
