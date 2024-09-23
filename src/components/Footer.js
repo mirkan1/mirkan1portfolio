d6f0d7b57234d75744354ac0ec8504e7bbc2b00d
mirkan-samsung-wsl <mirkanbaba1@gmail.com>
2024-09-23 21:59:11 +0700

    chore: Update footer links and styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import githubLogo from '../icons/GitHub-Mark-32px.png';
+import githubLogo from '../icons/github.png';
 import linkedinLogo from '../icons/linkedin.png';
+import facebookLogo from '../icons/facebook.png';
 import twitterLogo from '../icons/twitter.png';
 import "../css/fontawesome.all.min.css";
 import "../css/style.css";
@@ -11,17 +12,21 @@ export default function Footer() {
     <div className="borderer">
       <div className="copyright-div">
         <div className="footer-b">
-            <a href="https://github.com/mirkan1" target="_blank" rel="noopener noreferrer">
-          <img src={githubLogo} height="32" width="32" alt="media-link"></img>
-              github
-            </a>
-          <a href="https://twitter.com/mirkanOFmirkan" target="_blank" rel="noopener noreferrer">
-          <img src={twitterLogo} height="32" width="32" alt="media-link"></img>
+          <a href="https://github.com/mirkan1">
+            <img src={githubLogo} height="32" width="32" alt="media-link"></img>
+            github
+          </a>
+          <a href="https://twitter.com/mirkanOFmirkan">
+            <img src={twitterLogo} height="32" width="32" alt="media-link"></img>
             twitter
           </a>
-          <a className="linkedin" href="https://linkedin.com/in/raqux/" target="_blank" rel="noopener noreferrer">
-          <img src={linkedinLogo} height="32" width="32" alt="media-link"></img>
+          <a className="linkedin" href="https://linkedin.com/in/raqux">
+            <img src={linkedinLogo} height="32" width="32" alt="media-link"></img>
             linkedin
+          </a>
+          <a className="facebook" href="https://facebook.com/r.mirkan">
+            <img src={facebookLogo} height="32" width="32" alt="media-link"></img>
+            facebook
           </a>
         </div>
       </div>
